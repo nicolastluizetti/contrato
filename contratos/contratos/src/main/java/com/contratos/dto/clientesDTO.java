cf86e92d97c6a47d072ca951d712a9b78af27329
Nicolas Thomas Luizetti <96789314+nicolastluizetti@users.noreply.github.com>
2026-04-07 16:12:24 -0300

    Add files via upload
    adicionado melhorias ao exception handler e validacoes nos controllers

---
+++ contratos/contratos/src/main/java/com/contratos/dto/clientesDTO.java
@@ -0,0 +1,12 @@
+package com.contratos.dto;
+
+public record clientesDTO(
+		 	Integer id,
+		    String nome,
+		    Double cnpj,
+		    String email,
+		    Integer telefone
+		    
+	    ) {
+
+}
