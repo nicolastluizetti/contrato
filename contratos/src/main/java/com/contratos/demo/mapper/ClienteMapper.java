7924a124d07d1ee321e8c6193f49d6ffb5c97f45
Nicolas Thomas Luizetti <96789314+nicolastluizetti@users.noreply.github.com>
2026-06-27 12:41:00 -0300

    Update ClienteMapper.java

--- contratos/src/main/java/com/contratos/demo/mapper/ClienteMapper.java
+++ contratos/src/main/java/com/contratos/demo/mapper/ClienteMapper.java
@@ -9,23 +9,15 @@ import org.mapstruct.MappingTarget;
 import com.contratos.demo.DTO.ClienteDTO;
 import com.contratos.demo.model.Cliente;
 
-
-
 @Mapper(componentModel = "spring")
 public interface ClienteMapper {
 
-  
-    @Mapping(source = "cliente.id", target = "clienteId")
-    ClienteDTO toDTO(Cliente clientes);
+    ClienteDTO toDTO(Cliente cliente);
 
+    Cliente toEntity(ClienteDTO dto);
 
-    @Mapping(source = "clienteId", target = "cliente.id")
-    Cliente toEntity(ClienteDTO clientesdto);
-    
-    @Mapping(source = "clienteId", target = "cliente.id")
     List<ClienteDTO> toDTO(List<Cliente> clientes);
-    
+
     @Mapping(target = "id", ignore = true)
     void updateEntityFromDto(ClienteDTO dto, @MappingTarget Cliente entity);
 }
-
